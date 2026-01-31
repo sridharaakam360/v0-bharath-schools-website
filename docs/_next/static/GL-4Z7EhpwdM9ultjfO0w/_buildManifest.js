2febcb950070b0cfa74e49fd4cb658baeb730292
Vinothkumar0311 <svinothkumar0301@gmail.com>
2026-01-31 13:04:29 +0530

    Deploy site via GitHub Pages using docs folder

---
+++ docs/_next/static/GL-4Z7EhpwdM9ultjfO0w/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/bharath_HITECH//_next/:path+",
+        "destination": "/bharath_HITECH/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
