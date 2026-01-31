b51939182969b482e204acff91dd6c0fc4a5341c
Vinothkumar Sakthivel <91531509+Vinothkumar0311@users.noreply.github.com>
2026-01-31 14:57:46 +0530

    Refactor next.config.mjs for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,36 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   images: {
+//     unoptimized: true,
+//   },
+//  basePath: "/bharath_HITECH",
+//   assetPrefix: "/bharath_HITECH/",
+// }
+
+// export default nextConfig
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: "export",
+
+  // ⚠️ basePath and assetPrefix are ONLY for GitHub Pages deployment
+  // Comment them out for Render deployment
+  // basePath: "/bharath_HITECH",
+  // assetPrefix: "/bharath_HITECH/",
+
+  trailingSlash: true,
+
   images: {
     unoptimized: true,
   },
- 
-}
 
-export default nextConfig
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+export default nextConfig;
